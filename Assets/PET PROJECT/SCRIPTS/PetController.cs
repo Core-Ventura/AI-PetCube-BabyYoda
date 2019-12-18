9271324835a5af977594d91ea069b6e523d0c7d2
Core-Ventura <core.ventura@gmail.com>
2019-12-18 17:35:32 +0000

    Initial Commit

---
+++ Assets/PET PROJECT/SCRIPTS/PetController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetController : MonoBehaviour
+{
+    public Transform goal;
+    public float speed = 0.5f; //ajustar para la animacion
+    public float acc = 1.0f;
+    public float rotSpeed = 0.5f;
+
+    void LateUpdate() 
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x, 
+                                        this.transform.position.y,
+                                        goal.position.z);
+        Vector3 direction = lookAtGoal - this.transform.position;
+        
+        if (Vector3.Distance(transform.position, lookAtGoal) > acc){
+
+            //Rotate pet
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                            Quaternion.LookRotation(direction), 
+                                            Time.deltaTime*rotSpeed);
+
+            //Move pet
+            this.transform.Translate(0,0,speed*Time.deltaTime);
+        }
+  
+    }
+}
